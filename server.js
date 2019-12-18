eed9025bf27ce94ce8fda75829181a2ac85811c9
Rajesh KP <rajesh.kp@kpmg.aptaracorp.com>
2019-12-18 17:45:51 +0530

    static path change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 app.use(compression());
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-testing'));
 
 // Start the app by listening on the default Heroku port
 app.listen(port);
